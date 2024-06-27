cb1c15bab4ab40a06a17034bc94124cbe10e64f5
Haruki <aratashiharuki@gmail.com>
2024-06-28 00:39:06 +0300

    App:status exposed to react app

--- desktop/src/preload.js
+++ desktop/src/preload.js
@@ -3,4 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electron', {
   setServer: (state) => ipcRenderer.send('settings:server', state),
   onServerUpdate: (callback) => ipcRenderer.on('settings:server', (event, state) => callback(state)),
+
+  onStatusUpdate: (callback) => ipcRenderer.on('app:status', (event, state) => callback(state))
 });
