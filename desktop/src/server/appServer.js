b5fc92a94d80eb8ea08e66a176a9f17b3c7d76e4
Haruki <aratashiharuki@gmail.com>
2024-06-28 00:28:40 +0300

    Added status replication through IRPC on React app

--- desktop/src/server/appServer.js
+++ desktop/src/server/appServer.js
@@ -1,24 +1,45 @@
 const express = require('express')
+const MainWindow = require('../classes/mainWindow')
+const AppRouter = require('../routes/appRouter')
 
 class AppServer {
     #app
     #connection
+
     #httpPort
+    #status
 
     constructor(port) {
         this.#app = express()
         this.#app.use(express.json())
         this.#httpPort = port
+
+        this.#app.use(new AppRouter(this.#app).getRoutes())
+
+        this.#updateStatus(0)
+    }
+
+    getStatus() {
+        return this.#status
+    }
+
+    #updateStatus(newStatus) {
+        this.#status = newStatus
+        MainWindow.getWindow().webContents.send('app:status', newStatus)
     }
 
     start() {
-        this.#connection = this.#app.listen(this.#httpPort)
+        this.#connection = this.#app.listen(this.#httpPort, () => {
+            this.#updateStatus(1)
+        })
     }
 
     stop() {
         if (this.#connection) {
             this.#connection.close()
             this.#connection = null
+
+            this.#updateStatus(0)
         }        
     }
 }
