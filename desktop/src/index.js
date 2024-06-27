b86778589a036bc0dd8a1a4cdaa146aaae0d2545
Haruki <aratashiharuki@gmail.com>
2024-06-27 22:06:55 +0300

    Added isDev env variable, connect to React DEV server, included ipcListener

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -1,6 +1,13 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path');
 const AppServer = require('./server/appServer');
+const { config } = require('dotenv');
+const IpcListener = require('./server/ipcListener');
+
+config()
+
+const appServer = new AppServer(3011)
+const ipcListener = new IpcListener(appServer)
 
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -9,24 +16,26 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => { 
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 400,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.removeMenu()
 
-  mainWindow.webContents.openDevTools();
+  if (process.env.isDev) {
+    mainWindow.loadURL('http://localhost:3000')
+  } else {
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  }
 };
 
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  const test = new AppServer(3011)
-  test.start()
-
   createWindow();
+  ipcListener.listen()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
