134445eefacefc77a047761eb669a38b4dcf1c9f
Haruki <aratashiharuki@gmail.com>
2024-06-28 05:32:39 +0300

    Metrics and progress listeners

--- frontend/src/contexts/AppContext.js
+++ frontend/src/contexts/AppContext.js
@@ -4,16 +4,39 @@ const AppContext = createContext()
 
 const AppContextProvider = ({ children }) => {
     const [status, setStatus] = useState(0)
+    const [metrics, setMetrics] = useState()
+    const [progress, setProgress] = useState()
 
     useEffect(() => {
         window.electron.onStatusUpdate((status) => {
             setStatus(status)
         })
+
+        window.electron.onCanvasUpdate((metrics) => {
+            setMetrics([metrics.width, metrics.height])
+        })
+
+        window.electron.onProgressUpdate((progress) => {
+            setProgress(progress)
+        })
     }, [])
+
+    useEffect(() => {
+        if (status == 0 || status == 1) {
+            setMetrics(null)
+            setProgress(null)
+        }
+    }, [status])
 
     const payload = {
         status,
-        setStatus
+        setStatus,
+
+        metrics,
+        setMetrics,
+        
+        progress,
+        setProgress
     }
 
     return (
