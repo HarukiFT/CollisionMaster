1fe61c6e5470f5267e79a6217ab580768567d274
Haruki <aratashiharuki@gmail.com>
2024-06-28 04:42:57 +0300

    Added on status, canvas, progress update listeners

--- desktop/src/preload.js
+++ desktop/src/preload.js
@@ -4,5 +4,8 @@ contextBridge.exposeInMainWorld('electron', {
   setServer: (state) => ipcRenderer.send('settings:server', state),
   onServerUpdate: (callback) => ipcRenderer.on('settings:server', (event, state) => callback(state)),
 
-  onStatusUpdate: (callback) => ipcRenderer.on('app:status', (event, state) => callback(state))
+  onStatusUpdate: (callback) => ipcRenderer.on('app:status', (event, state) => callback(state)),
+  
+  onCanvasUpdate: (callback) => ipcRenderer.on('render:new_canvas', (event, metrics) => callback(metrics)),
+  onProgressUpdate: (callback) => ipcRenderer.on('render:new_progress', (event, progress) => callback(progress))
 });
