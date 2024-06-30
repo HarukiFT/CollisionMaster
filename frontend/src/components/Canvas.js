1b925b3dd3fb216808092a8ac184df9fdf4f6928
Haruki <aratashiharuki@gmail.com>
2024-07-01 02:25:26 +0300

    Added movement logic, color depends from depth

--- frontend/src/components/Canvas.js
+++ frontend/src/components/Canvas.js
@@ -1,29 +1,105 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useAppContext } from "../contexts/AppContext"
+import Styles from '../styles/Canvas.module.scss'
 
 export default () => {
     const appContext = useAppContext()
-    const canvasRef = useRef()
+    const canvasRef = useRef(null)
 
-    useEffect(() => {
-        const context = canvasRef.current.getContext('2d')
+    const [scale, setScale] = useState(1)
+    const [offset, setOffset] = useState({ x: 0, y: 0 })
+    const [dragging, setDragging] = useState(false)
+    const [startOffset, setStartOffset] = useState(null)
+    const [minMax, setMinMax] = useState([])
 
-        console.log(appContext.canvasData)  
+    const drawCanvas = () => {
+        console.log(scale)
+
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+
+        const minValue = minMax[0]
+        const maxValue = minMax[1]
+
+        context.imageSmoothingEnabled = false;
+        context.setTransform(scale, 0, 0, scale, offset.x, offset.y)
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        context.clearRect(-offset.x / scale, -offset.y / scale, canvas.width / scale, canvas.height / scale)
+
         for (let i = 0; i < appContext.canvasData.length; i++) {
             for (let j = 0; j < appContext.canvasData[i].length; j++) {
-                console.log(appContext.canvasData[i][j])
-                context.fillStyle = appContext.canvasData[i][j] ? '#009900' : '#000'
+                if (appContext.canvasData[i][j]) {
+                    const normalizedValue = (appContext.canvasData[i][j] - minValue) / (maxValue - minValue)
+                    const shade = Math.floor(200 * normalizedValue)
+                    context.fillStyle = `rgb(0, ${255 - shade}, 0)`
+                } else {
+                    context.fillStyle = `rgb(255, 0, 0)`
+                }
+
                 context.fillRect(i, j, 1, 1)
             }
-        } 
+        }
+    }
+
+    useEffect(() => {
+        const values = appContext.canvasData.flat().filter(value => value !== undefined && value !== null)
+        const minValue = Math.min(...values)
+        const maxValue = Math.max(...values)
+
+        setMinMax([minValue, maxValue])
+
+        drawCanvas()
     }, [appContext.canvasData])
 
-    return (
-        <canvas ref={canvasRef} style={{
-            width:  appContext.canvasData[0].length,
-            height: appContext.canvasData.length
-        }}>
+    useEffect(() => {
+        drawCanvas()
+    }, [offset, scale])
 
-        </canvas>
+    const handleMouseDown = (event) => {
+        setDragging(true)
+        setStartOffset({ x: event.clientX, y: event.clientY })
+    }
+
+    const handleMouseMove = (event) => {
+        if (!dragging) return
+
+        const newOffsetX = offset.x + (event.clientX - startOffset.x)
+        const newOffsetY = offset.y + (event.clientY - startOffset.y)
+
+        setOffset({
+            x: newOffsetX,
+            y: newOffsetY,
+        })
+
+        setStartOffset({ x: event.clientX, y: event.clientY })
+    }
+
+    const handleMouseUp = () => {
+        setDragging(false)
+        setStartOffset(null)
+    }
+
+    const handleWheel = (event) => {
+        const delta = Math.sign(event.deltaY)
+        const scaleFactor = .5
+        let newScale = scale
+
+        if (delta > 0) {
+            newScale = Math.max(.5, scale - scaleFactor)
+        } else {
+            newScale = scale + scaleFactor
+        }
+
+        setScale(newScale)
+    }
+
+    return (
+        <div className={Styles.canvasWrapper}
+            onWheel={handleWheel}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}>
+                <canvas ref={canvasRef}/>
+        </div>
     )
 }
