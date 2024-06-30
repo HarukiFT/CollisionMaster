2a57079c75e7268e4c0ea8aa1ddd0f4370c1ecab
Haruki <aratashiharuki@gmail.com>
2024-06-30 09:27:37 +0300

    Added onDataUpdate for final canvas data after finish

--- desktop/src/preload.js
+++ desktop/src/preload.js
@@ -5,7 +5,8 @@ contextBridge.exposeInMainWorld('electron', {
   onServerUpdate: (callback) => ipcRenderer.on('settings:server', (event, state) => callback(state)),
 
   onStatusUpdate: (callback) => ipcRenderer.on('app:status', (event, state) => callback(state)),
-  
+  onDataUpdate: (callback) => ipcRenderer.on('app:canvas_data', (event, data) => callback(data)),
+
   onCanvasUpdate: (callback) => ipcRenderer.on('render:new_canvas', (event, metrics) => callback(metrics)),
-  onProgressUpdate: (callback) => ipcRenderer.on('render:new_progress', (event, progress) => callback(progress))
+  onProgressUpdate: (callback) => ipcRenderer.on('render:new_progress', (event, progress) => callback(progress)),
 });
