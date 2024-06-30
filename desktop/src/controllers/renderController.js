9b7198ab92b849093285170e4b396ae5dca3ff7a
Haruki <aratashiharuki@gmail.com>
2024-06-30 09:26:19 +0300

    Handle multiset of pixels

--- desktop/src/controllers/renderController.js
+++ desktop/src/controllers/renderController.js
@@ -7,11 +7,11 @@ class RenderController {
 
     handleCreate(req, res) {
         // Projecting on 2D
-        const { a, b } = req.body
-        const width = Math.abs(a.x - b.x)
-        const height = Math.abs(a.z - b.z)
+        const { a, b, settings } = req.body
+        const width = Math.ceil(Math.abs(a.x - b.x))
+        const height = Math.ceil(Math.abs(a.z - b.z))
 
-        const result = this.#appServer.createCanvas(width, height)
+        const result = this.#appServer.createCanvas(width, height, settings)
 
         if (result) {
             return res.sendStatus(200)
@@ -30,6 +30,18 @@ class RenderController {
         }
         
         return res.sendStatus(403)
+    }
+
+    handleMultiset(req, res) {
+        for (let pixelData of req.body) {
+            const result = this.#appServer.setPixel(pixelData.x, pixelData.y, pixelData.depth)
+
+            if (!result) {
+                return res.sendStatus(403)
+            }
+        }
+
+        res.sendStatus(200)
     }
 }
 
